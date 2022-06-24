b540a94d81a135cec9329d93db75b58bacb229f5
Alessio Onori <alessio.onori@gmail.com>
2022-06-24 14:28:01 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,6 @@ source /scripts/common.sh
 helm repo update
 # helmfile --environment $HELM_ENV -f ./helmfile.pre.d sync
 
-wait_pod_ready cert-manager-webhook cert-manager
+# wait_pod_ready cert-manager-webhook cert-manager
 
 /scripts/deploy.sh -c ${CLUSTER}
